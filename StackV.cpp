2edd391eba2003055263af8ef93cfa24190e0a95
KingDuke0187 <KingDuke0187@users.noreply.github.com>
2019-04-04 16:01:46 -0400

    Added source file

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,30 @@
 #include "StackV.h"
+#include <iostream>
+#include <vector>
+#include <stdexcept> // use to throw
+
+int Stack::size()
+{
+	return data.size();
+}
+void Stack::push(int x)
+{
+	return data.push_back(x)
+}
+void Stack::pop()
+{
+	data.pop_back();
+}
+int Stack::top()
+{
+	int i=0;
+	i =data.size()-1;
+	return data[i];
+}
+void Stack::clear()
+{
+	while (size() !=0)
+	data.pop_back();
+}
+
+
